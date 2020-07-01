8131fcc920d089e413846d6399e8db7a35dd21a1
Ivandeclei <ivandeclei@gmail.com>
2020-07-01 17:19:22 -0300

    department service

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,10 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 
 public class DepartmentListController implements Initializable{
 
+	private DepartmentService service; //vincula controller com service de model
 	@FXML
 	private TableView<Department> tableViewDepartment;
 	
@@ -28,8 +33,15 @@ public class DepartmentListController implements Initializable{
 	@FXML
 	private Button btNew;
 	
+	private ObservableList<Department> obsList;
+	
 	public void onBtNewAction() {
 		System.out.println("btn");
+	}
+	
+	//injeta a dependencia sem a necessidade de instanciar o objeto direto na classe e eliminiand o forte acoplamento
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@Override
@@ -46,6 +58,15 @@ public class DepartmentListController implements Initializable{
 		Stage stage = (Stage)Main.getMainScene().getWindow(); //pega tamnho da tela pricipal fazendo um downcasting para stage
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty()); //atribui o tamnho anterior para o tableview 
 		
+	}
+	
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Service esta null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
 	}
 
 }
