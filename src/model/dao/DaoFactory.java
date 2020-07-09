bdae6f5ae2c275c3499b6c1042abc9bf77fc8052
Ivandeclei <ivandeclei@gmail.com>
2020-07-09 15:13:22 -0300

    adicionando acesso a banco de dados

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,18 @@
+package model.dao;
+
+import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	//classe responsavel por instancial os Daos
+	
+	// retorna o tipo da interface  mas mas instancia um seller dao jdbc 
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
+}
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -2,16 +2,14 @@ package model.entities;
 
 import java.io.Serializable;
 
-public class Department implements Serializable {
+public class Department implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
 	private Integer id;
 	private String name;
-	
 	public Department() {
+		
 	}
-
 	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
@@ -32,7 +30,6 @@ public class Department implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -40,7 +37,6 @@ public class Department implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -57,9 +53,9 @@ public class Department implements Serializable {
 			return false;
 		return true;
 	}
-
 	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
+	
 }
